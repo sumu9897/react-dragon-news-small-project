a29675cba86744fb6c4f241292c578f07bee4fb5
MOHAMMAD SUMON <mohammad.sumon9897@gmail.com>
2024-11-15 14:44:55 +0600

    Registration Data Get Console

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,12 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import userIcon from '../assets/user.png'
+import { AuthContext } from '../provider/AuthProvider';
 const Navbar = () => {
+    const {user} = useContext(AuthContext)
     return (
         <div className='flex justify-between items-center'>
-            <div></div>
+            <div>{ user && user.name }</div>
             <div className='nav space-x-5'>
                 <Link to="/">Home</Link>
                 <Link to="/career">Career</Link>
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,37 +1,47 @@
 import { Link } from "react-router-dom";
 
 const Register = () => {
+    const handleSubmit=(e)=>{
+        e.preventDefault();
+        // get form data
+        const form= new FormData(e.target)
+        const name = form.get("name");
+        const email = form.get("email");
+        const photo = form.get("photo");
+        const password = form.get("password");
+        console.log(name, email,photo,password)
+    }
     return (
         <div className='min-h-screen flex justify-center items-center'>
             <div className="card bg-base-100 w-full max-w-lg shrink-0 shadow-2xl p-10">
                 <h2 className='text-2xl font-semibold text-center'>Register your Account</h2>
-                <form className="card-body">
+                <form onSubmit={handleSubmit} className="card-body">
                     {/* name input */}
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text">Your Name</span>
                     </label>
-                    <input type="text" placeholder="Enter your name" className="input input-bordered" required />
+                    <input name="name" type="text" placeholder="Enter your name" className="input input-bordered" required />
                     </div>
                     {/* photo url */}
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text">Photo URL</span>
                     </label>
-                    <input type="text" placeholder="photo-url" className="input input-bordered" required />
+                    <input name="photo" type="text" placeholder="photo-url" className="input input-bordered" required />
                     </div>
                     {/* email input */}
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text">Email address</span>
                     </label>
-                    <input type="email" placeholder="Enter your email address" className="input input-bordered" required />
+                    <input name="email" type="email" placeholder="Enter your email address" className="input input-bordered" required />
                     </div>
                     <div className="form-control">
                     <label className="label">
                         <span className="label-text">Password</span>
                     </label>
-                    <input type="password" placeholder="Enter your password" className="input input-bordered" required />
+                    <input name="password" type="password" placeholder="Enter your password" className="input input-bordered" required />
                     <label className="label">
                         <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
                     </label>
